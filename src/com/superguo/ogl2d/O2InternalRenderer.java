d4afe731c711dce0bfd178027f3ff78b8801656a
supersuperguo@gmail.com <supersuperguo@gmail.com@b5bdd040-67ce-22ae-f1ba-61700bf9d858>
2011-03-16 08:04:44 +0000

    1. Fixes the image in texture that is not power of 2 2. Implements some onXXX() code

--- src/com/superguo/ogl2d/O2InternalRenderer.java
+++ src/com/superguo/ogl2d/O2InternalRenderer.java
@@ -3,6 +3,8 @@ package com.superguo.ogl2d;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.opengl.GLES10;
+import android.opengl.GLES11;
 import android.opengl.GLSurfaceView;
 
 class O2InternalRenderer implements GLSurfaceView.Renderer{
@@ -13,20 +15,19 @@ class O2InternalRenderer implements GLSurfaceView.Renderer{
 		this.director = director;
 	}
 
-	public void onDrawFrame(GL10 arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) {
+		GLES10.glEnableClientState(GLES10.GL_VERTEX_ARRAY);
+		GLES10.glEnable(GLES10.GL_TEXTURE_2D);
+		GLES10.glDisable(GLES10.GL_DITHER);
 	}
-
+	
 	public void onSurfaceChanged(GL10 arg0, int arg1, int arg2) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) {
+	public void onDrawFrame(GL10 arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-
 }
--- src/com/superguo/ogl2d/O2Sprite.java
+++ src/com/superguo/ogl2d/O2Sprite.java
@@ -1,19 +1,22 @@
 package com.superguo.ogl2d;
 import java.nio.*;
 
-import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.opengles.*;
 
 import android.graphics.Bitmap;
 import android.opengl.*;
 
 public abstract class O2Sprite {
+	public final static int MAX_SIZE = 1024;
 	protected boolean available;
 	protected boolean managed;
 	protected int tex;
 	protected int width;
 	protected int height;
-	private final static int fullTexCoods[] = {0, 0, 1, 0, 0, 1, 1, 1};
-	private final static IntBuffer fullTexBuf = IntBuffer.wrap(fullTexCoods);
+	protected int texWidth;
+	protected int texHeight;
+	protected int vboFullTexCood;
+
 	private float vertCoods[] = new float[8];
 	private float texCoods[] = new float[8];
 	private FloatBuffer vertBuf = FloatBuffer.allocate(8);
@@ -43,14 +46,47 @@ public abstract class O2Sprite {
 		{
 			int texArr[] = { tex };
 			GLES10.glDeleteTextures(1, texArr, 0);
+			int vboArray[] = {	vboFullTexCood };
+			GLES11.glDeleteBuffers(0, vboArray, 0);
 		}
 		available = false;
 	}
 
 	protected void createTexFromBitmap(Bitmap bmp)
 	{
-		width = bmp.getWidth();
-		height = bmp.getHeight();
+		int w = bmp.getWidth();
+		int h = bmp.getHeight();
+		if (w > MAX_SIZE || h > MAX_SIZE) throw new Error("bitmap width/height larger than 1024");
+		if (w <=0 || h <= 0) throw new IllegalArgumentException("bitmap width/height <= 0");
+
+		// pad the size of bitmap to the power of 2
+		int actualW = w;
+		int actualH = h;
+		int powOf2;
+
+		for (powOf2=0; actualW>1; actualW>>=1) powOf2++;
+		actualW = (1 << powOf2);
+		if (w != actualW) actualW<<=1;
+
+		for (powOf2=0; actualH>1; actualH>>=1) powOf2++;
+		actualH = (1 << powOf2);
+		if (h != actualH) actualH<<=1;
+		
+		if (w != actualW || h != actualH)
+		{
+			Bitmap standardBmp = Bitmap.createBitmap(bmp, 0, 0, actualW, actualH);
+			createTexFromStandardBitmap(standardBmp, w, h);
+		}
+		else
+			createTexFromStandardBitmap(bmp, w, h);
+	}
+	
+	private void createTexFromStandardBitmap(Bitmap bmp, int width, int height)
+	{
+		texWidth = bmp.getWidth();
+		texHeight = bmp.getHeight();
+		this.width = width;
+		this.height = height;
 
 		int texArr[] = new int[1];
 		GLES10.glGenTextures(1, texArr, 0);
@@ -70,10 +106,21 @@ public abstract class O2Sprite {
         //GLES10.glTexEnvf(GLES10.GL_TEXTURE_ENV, GLES10.GL_TEXTURE_ENV_MODE,
         	//	GLES10.GL_REPLACE); 
 		GLUtils.texImage2D(GLES10.GL_TEXTURE_2D, 0, bmp, 0);
+		
+		int[] vboArr = new int[1];
+		float fullTexCoods[] = {0.0f, 0.0f, (float)width/texWidth, 0.0f, 0.0f, (float)height/texHeight, (float)width/texWidth, (float)height/texHeight};
+		FloatBuffer fullTexBuf = FloatBuffer.wrap(fullTexCoods);
+
+		GLES11.glGenBuffers(1, vboArr, 0);
+		vboFullTexCood = vboArr[0];
+		GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, vboFullTexCood);
+		GLES11.glBufferData(GLES11.GL_ARRAY_BUFFER, 8, fullTexBuf, GLES11.GL_STATIC_DRAW);
+		GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, 0);
 	}
-	
+
 	public final void draw(int tagetX, int targetY)
 	{
+		// vertex coordinations
 		vertCoods[0] = vertCoods[1] = vertCoods[3] = vertCoods[4] = 0;
 		vertCoods[2] = vertCoods[6] = width;
 		vertCoods[5] = vertCoods[7] = height;
@@ -81,11 +128,16 @@ public abstract class O2Sprite {
 		vertBuf.position(0);
 		vertBuf.put(vertCoods);
 		vertBuf.position(0);
-	
-		GL10 gl = O2Director.getInstance().gl;
-		gl.glVertexPointer(2, GLES10.GL_FLOAT, 0, vertBuf);
-		gl.glTexCoordPointer(2, GLES10.GL_FIXED, 0, fullTexBuf);
 		
+		GL10 gl = O2Director.instance.gl;
+		gl.glVertexPointer(2, GLES10.GL_FLOAT, 0, vertBuf);
+		
+		// texture coordinations
+		GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, vboFullTexCood);
+		GLES11.glTexCoordPointer(2, GLES10.GL_FLOAT, 0, 0);
+		GLES11.glBindBuffer(GLES11.GL_ARRAY_BUFFER, 0);
+
+		// draw
 		GLES10.glDrawArrays(GLES10.GL_TRIANGLE_STRIP, 0, 4);
 	}
 	
@@ -100,16 +152,16 @@ public abstract class O2Sprite {
 		vertBuf.put(vertCoods);
 		vertBuf.position(0);
 		
-		texCoods[0] = texCoods[4] = (float)srcX / width;
-		texCoods[1] = texCoods[3] = (float)srcY / height;
-		texCoods[2] = texCoods[6] =  (float)(srcX + srcWidth) / width;
-		texCoods[5] = texCoods[7] =  (float)(srcY + srcHeight) / height;
+		texCoods[0] = texCoods[4] = (float)srcX / texWidth;
+		texCoods[1] = texCoods[3] = (float)srcY / texHeight;
+		texCoods[2] = texCoods[6] =  (float)(srcX + srcWidth) / texWidth;
+		texCoods[5] = texCoods[7] =  (float)(srcY + srcHeight) / texHeight;
 		
 		texBuf.position(0);
 		texBuf.put(texCoods);
 		texBuf.position(0);
 	
-		GL10 gl = O2Director.getInstance().gl;
+		GL10 gl = O2Director.instance.gl;
 		gl.glVertexPointer(2, GLES10.GL_FLOAT, 0, vertBuf);
 		gl.glTexCoordPointer(2, GLES10.GL_FIXED, 0, texBuf);
 		

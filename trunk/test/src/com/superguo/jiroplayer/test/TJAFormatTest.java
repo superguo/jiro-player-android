e02ef2ca543e235fae79278fbc90326b46876f54
supersuperguo@gmail.com <supersuperguo@gmail.com@b5bdd040-67ce-22ae-f1ba-61700bf9d858>
2011-04-13 15:21:50 +0000

    Game: 1. Added a test project to test TJAFormat

---
+++ trunk/test/src/com/superguo/jiroplayer/test/TJAFormatTest.java
@@ -0,0 +1,41 @@
+package com.superguo.jiroplayer.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.superguo.jiroplayer.*;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.test.*;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.util.Log;
+
+public class TJAFormatTest extends ActivityUnitTestCase<TestActivity> {
+	private Intent iStartIntent;  
+	
+	public TJAFormatTest() throws IOException {
+		super(TestActivity.class);
+
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		iStartIntent = new Intent(Intent.ACTION_MAIN);  
+	}
+	
+	@MediumTest
+	public void testTJAFormat1() throws IOException
+	{
+		TestActivity ac = startActivity(iStartIntent, null, null);
+		InputStream is = ac.getAssets().open("sample.tja", AssetManager.ACCESS_RANDOM);
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		TJAFormat tjaFormat = new TJAFormat(br);
+		Log.d("jiro-test", "wave=" + tjaFormat.iWave);
+		br = null;
+		is.close();
+	}
+}

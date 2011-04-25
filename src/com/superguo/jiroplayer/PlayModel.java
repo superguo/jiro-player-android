404e280bb786e768df414164bb5b58dcc552ba98
supersuperguo@gmail.com <supersuperguo@gmail.com@b5bdd040-67ce-22ae-f1ba-61700bf9d858>
2011-04-25 14:57:31 +0000

    Game: 1. Continued to implement PlayModel

--- src/com/superguo/jiroplayer/PlayModel.java
+++ src/com/superguo/jiroplayer/PlayModel.java
@@ -58,18 +58,41 @@ public class PlayModel {
 	
 	private int iScore;
 	private long iSysStartTime;
+	private DisplayInfo iDisplayInfo = new DisplayInfo();
 	
 	public static class DisplayInfo
 	{
-		public final static int DISPLAY_SEPARATOR	= 0;
-		public final static int DISPLAY_RED 		= 1;
-		public final static int DISPLAY_BLUE 		= 2;
-		public final static int DISPLAY_BIG_RED 	= 3;
-		public final static int DISPLAY_BIG_BLUE 	= 4;
-		public final static int DISPLAY_LENDA_HEAD 	= 5;
-		public final static int DISPLAY_LENDA_BODY 	= 6;
-		public final static int DISPLAY_BALOON_HEAD	= 7;
-		public final static int DISPLAY_BALOON_BODY	= 8;
+		public final static int MAX_NOTE_POS = 50;
+		public final static int NOTE_SEPARATOR		= 0;
+		public final static int NOTE_FACE 			= 1;
+		public final static int NOTE_SIDE 			= 2;
+		public final static int NOTE_BIG_FACE 		= 3;
+		public final static int NOTE_BIG_SIDE 		= 4;
+		public final static int NOTE_LENDA_HEAD 	= 5;
+		public final static int NOTE_LENDA_BODY 	= 6;
+		public final static int NOTE_LENDA_TAIL 	= 6;
+		public final static int NOTE_BIG_LENDA_HEAD = 7;
+		public final static int NOTE_BIG_LENDA_BODY = 8;
+		public final static int NOTE_BIG_LENDA_TAIL = 9;
+		public final static int NOTE_BALOON_HEAD	= 10;
+		public final static int NOTE_BALOON_BODY	= 11;
+		
+		public final static int BRANCH_NONE			= 0;
+		public final static int BRANCH_EASY			= 1;
+		public final static int BRANCH_NORMAL		= 2;
+		public final static int BRANCH_MASTER		= 3;
+		
+		public static class NotePos
+		{
+			public int iNoteType;
+			public int iNotePos;
+		}
+		
+		public int iNumNotePos;
+		public NotePos[] iNotePosArray = new NotePos[MAX_NOTE_POS];
+		public int iBranch;
+		public boolean iIsGGT;
+		public int iBaloonLenDaCount;
 	}
 
 	public void prepare(TJAFormat aTJA, int aCourseIndex)

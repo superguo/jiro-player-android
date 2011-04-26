8853178c81b9061a9a4cb67f2027b2d6cd0c50e5
supersuperguo@gmail.com <supersuperguo@gmail.com@b5bdd040-67ce-22ae-f1ba-61700bf9d858>
2011-04-26 15:03:43 +0000

    Game: 1. Continued to implement PlayModel

---
+++ src/com/superguo/jiroplayer/PlayDisplayInfo.java
@@ -0,0 +1,36 @@
+package com.superguo.jiroplayer;
+
+public class PlayDisplayInfo
+{
+	public final static int MAX_NOTE_POS = 50;
+	public final static int NOTE_SEPARATOR		= 0;
+	public final static int NOTE_FACE 			= 1;
+	public final static int NOTE_SIDE 			= 2;
+	public final static int NOTE_BIG_FACE 		= 3;
+	public final static int NOTE_BIG_SIDE 		= 4;
+	public final static int NOTE_LENDA_HEAD 	= 5;
+	public final static int NOTE_LENDA_BODY 	= 6;
+	public final static int NOTE_LENDA_TAIL 	= 6;
+	public final static int NOTE_BIG_LENDA_HEAD = 7;
+	public final static int NOTE_BIG_LENDA_BODY = 8;
+	public final static int NOTE_BIG_LENDA_TAIL = 9;
+	public final static int NOTE_BALOON_HEAD	= 10;
+	public final static int NOTE_BALOON_BODY	= 11;
+	
+	public final static int BRANCH_NONE			= 0;
+	public final static int BRANCH_EASY			= 1;
+	public final static int BRANCH_NORMAL		= 2;
+	public final static int BRANCH_MASTER		= 3;
+	
+	public static class NotePos
+	{
+		public int iNoteType;
+		public int iNotePos;
+	}
+	
+	public int iNumNotePos;
+	public NotePos[] iNotePosArray = new NotePos[MAX_NOTE_POS];
+	public int iBranch;
+	public boolean iIsGGT;
+	public int iBaloonLenDaCount;
+}

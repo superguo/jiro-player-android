15222bcd03c342b36c8ba7772a25a03b1fed9364
supersuperguo@gmail.com <supersuperguo@gmail.com@b5bdd040-67ce-22ae-f1ba-61700bf9d858>
2011-05-04 18:23:35 +0000

    Game: 1. Continued to implement PlayModel

--- src/com/superguo/jiroplayer/PlayDisplayInfo.java
+++ src/com/superguo/jiroplayer/PlayDisplayInfo.java
@@ -27,13 +27,36 @@ public final class PlayDisplayInfo
 	public final static int BRANCH_NORMAL		= 2;
 	public final static int BRANCH_MASTER		= 3;
 	
+	public final static int ROLLING_NONE		= 0;
+	public final static int ROLLING_BAR			= 1;
+	public final static int ROLLING_BALLOON		= 2;
+	public final static int ROLLING_POTATO		= 3;
+	
+	public int iScore;
+	public int iAddedScore;
+
 	public int iNumNotePos;
 	public NotePos[] iNotePosArray = new NotePos[MAX_NOTE_POS];
 	public int iBranch;
 	public boolean iIsGGT;
-	public int iBallonLenDaCount;	// balloon down counter   
-									// 0 means just finished
-									// -1 means failed
-									// -2 means no lenda
+	public int iRollingState;		
+	public int iRollingCount;	// balloon/potato down counter / bar up counter
+								// balloon/potato:
+								// 0 means just finished
+								// -1 means failed
+								// -2 means no lenda
 	public int iNumCombo;
+	
+	public void reset()
+	{
+		// reset current score
+		iScore = 0;
+		iAddedScore = 0;
+		
+		iNumNotePos = 0;
+		iBranch = BRANCH_NONE;
+		iIsGGT = false;
+		iRollingState = ROLLING_NONE;
+		iRollingCount = 0;
+	}
 }
